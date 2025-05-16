fc841a8b6cf3422c3f0193098cc7fea3dca5581c
nffctaku <footballtopjp96@gmail.com>
2025-05-17 07:50:46 +0900

    fix: disableOptimizedLoadingでVercelエラー回避

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // 不要な experimental オプションは削除する
   experimental: {
     disableOptimizedLoading: true,
   },
 };
 
 export default nextConfig;
-
