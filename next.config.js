05cb706ed4ca256d8f2947ee4c581560ef313342
nffctaku <footballtopjp96@gmail.com>
2025-05-17 20:54:06 +0900

    feat: add next.config.js with headers for mobile auth

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
