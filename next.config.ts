f5ad093dcb5facfe7b930499b3806df92aacaba7
nffctaku <footballtopjp96@gmail.com>
2025-05-17 11:36:55 +0900

    fix: next.config.ts を安定版に修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // 不要な experimental オプションは削除する
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     disableOptimizedLoading: true,
   },
